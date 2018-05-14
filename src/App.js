adcee8cf7a654a9df06eee173cb608673ae6cca9
jujuck <julien.y.richard@gmail.com>
2018-05-14 16:50:38 +0200

    sauvegarde fin de journee Arinfo

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ class App extends Component {
       <div >
         <Layout>
           <Switch>
-            <Route path="/" exact component={BurgerBuilder} />
             <Route path="/checkout" component={Checkout} />
+            <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
       </div>
